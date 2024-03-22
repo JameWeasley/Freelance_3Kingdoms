eaed9eee9a6112663d08ecaa115c257eca6d1cd7
JameWeasley <101082867+JameWeasley@users.noreply.github.com>
2024-03-22 13:28:04 +0700

    a

--- src/page/item/addon/weapon.jsx
+++ src/page/item/addon/weapon.jsx
@@ -3,8 +3,28 @@ import React from 'react'
 export default function weapon() {
 
   const showWeapon = () => {
-   const wpn =  document.getElementById('show')
+    const wpn = document.getElementById('show')
     wpn.style.display = 'block'
+  }
+
+  const close = () => {
+    const cl = document.querySelector('.weapon-detail')
+    cl.style.display = "none"
+
+  }
+
+  let currentIndex = 0; // เริ่มต้นที่ index 0 (menu-list-info)
+
+  const changeList = () => {
+    const components = document.querySelectorAll('.menu-list-info, .menu-list-monster, .menu-list-store');
+    components[currentIndex].style.display = 'none'; // ซ่อน element ปัจจุบัน
+
+    currentIndex++; // เพิ่ม index ให้ไปยังตัวถัดไป
+    if (currentIndex >= components.length) { // ถ้าเกินจำนวน elements ทั้งหมดให้กลับไปที่ index 0
+      currentIndex = 0;
+    }
+
+    components[currentIndex].style.display = 'flex'; // แสดง element ใหม่
   }
 
   return (
@@ -14,10 +34,10 @@ export default function weapon() {
         <div className="menu-list" style={{ display: 'flex', justifyContent: 'center', margin: '5rem 0' }}>
           <div className="menu-list-detail">
             <div className="menu-select" style={{ textAlign: 'center' }}>
-              <select style={{ textAlign: 'center', width: '100%', border: 'none', outline: 'none', borderRadius: '5px', padding: '5px' }} >
-                <option name="ไอเทม">ไอเทม</option>
-                <option name="มอนส์เตอร์">มอนส์เตอร์</option>
-                <option name="ร้านค้า">ร้านค้า</option>
+              <select onChange={changeList} style={{ textAlign: 'center', width: '100%', border: 'none', outline: 'none', borderRadius: '5px', padding: '5px' }} >
+                <option value="ไอเทม">ไอเทม</option>
+                <option value="มอนส์เตอร์">มอนส์เตอร์</option>
+                <option value="ร้านค้า">ร้านค้า</option>
               </select>
             </div>
 
@@ -42,7 +62,22 @@ export default function weapon() {
               <span>ทั้งหมด</span>
               <span>ทั้งหมด</span>
             </div>
+
+            <div className="menu-list-monster" style={{ display: 'none', flexDirection: 'column', marginTop: '1rem', }}>
+              <span>ระดับ 1 - 50</span>
+              <span>ระดับ 51 - 100</span>
+              <span>ระดับ 101 - 150</span>
+              <span>ระดับ 151 - 200</span>
+              <span>ระดับ 201 - 250</span>
+              <span>ระดับ 251 - 300</span>
+            </div>
+
+            <div className="menu-list-store" style={{ display: 'none' }}>
+              <input type="search" />
+            </div>
           </div>
+
+
 
           <div className="item-list" style={{ marginLeft: '5rem', display: 'grid', gridTemplateColumns: 'repeat(5, 1fr)', gridGap: '20px' }}>
             <div className="item-blog">
@@ -209,11 +244,15 @@ export default function weapon() {
           </div>
         </div>
 
-        <div className="weapon-detail" id='show' style={{position: 'absolute', transform: 'translate(-50%, -50%)', top: '50%', left: '50%'}}>
+        <div className="weapon-detail" id='show' style={{ position: 'absolute', transform: 'translate(-50%, -50%)', top: '50%', left: '50%' }}>
           <div className="wep-head">
             <h3>รายละเอียด</h3>
             <h5>กระบี่</h5>
+
+
+
           </div>
+          <button className='btn btn-dark' onClick={close}>ปิด</button>
         </div>
 
       </div>
